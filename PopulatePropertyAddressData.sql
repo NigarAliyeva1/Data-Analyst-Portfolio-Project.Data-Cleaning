b80a5fdb8fcd45c7eb0ca22232aa6fd8b745aebe
Nigarkhanim Aliyeva <112957859+NigarAliyeva1@users.noreply.github.com>
2024-02-23 00:32:34 +0400

    Create PopulatePropertyAddressData.sql

---
+++ PopulatePropertyAddressData.sql
@@ -0,0 +1,25 @@
+-- Populate Property Address Data
+
+--Find Nulls with Self Join
+Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress
+From PortfolioProject.dbo.NashvilleHousing a
+JOIN PortfolioProject.dbo.NashvilleHousing b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress is null
+
+--Add ISNULL function
+Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress,b.PropertyAddress)
+From PortfolioProject.dbo.NashvilleHousing a
+JOIN PortfolioProject.dbo.NashvilleHousing b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress is null
+
+--Populate nulls with Property Address Data
+Update a
+SET PropertyAddress = ISNULL(a.PropertyAddress,b.PropertyAddress)
+From PortfolioProject.dbo.NashvilleHousing a
+JOIN PortfolioProject.dbo.NashvilleHousing b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
