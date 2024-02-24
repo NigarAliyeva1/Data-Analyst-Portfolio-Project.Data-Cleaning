bfd79df8ecc21c71eb7012b40f4477d58aa319b7
Nigarkhanim Aliyeva <112957859+NigarAliyeva1@users.noreply.github.com>
2024-02-24 19:00:31 +0400

    Create DeleteUnusedColumns.sql

---
+++ DeleteUnusedColumns.sql
@@ -0,0 +1,8 @@
+-- Delete Unused Columns
+
+Select *
+From PortfolioProject.dbo.NashvilleHousing
+
+
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
+DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress
